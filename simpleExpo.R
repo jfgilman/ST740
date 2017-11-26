4db4078415e46384c8b47151432a04a299bba107
jfgilman <jfgilman@ncsu.edu>
2017-11-26 11:44:18 -0500

    added simple version of expo McMC

---
+++ simpleExpo.R
@@ -0,0 +1,139 @@
+
+simpExpoMCMC <- function(data, samples = 5000, shapePriorA = .001,
+                     shapePriorB = .001, lamPriorA = .001, lamPriorB = .001,
+                     theta1PriorA = .001, theta1PriorB = .001, theta2PriorA = .001,
+                     theta2PriorB = .001, theta1Start = 1, theta2Start = 1, tuning = 1,
+                     burnin = 1000, thin = 10){
+  
+  # matrix for keeping MCMC draws for each parameter
+  lam_draws <- rep(0, samples)
+  lam_draws[1] <- 1
+  
+  theta1_draws <- rep(0, samples)
+  theta1_draws[1] <- theta1Start
+  
+  theta2_draws <- rep(0, samples)
+  theta2_draws[1] <- theta2Start
+  
+  # log posterior function for MCMC draws
+  logPost <- function(parm, d) {
+    
+    lp <- 0
+    for(k in 1:length(d)){
+      lp <- lp + 
+        sum(dexp(d[[k]]$MBF[which(d[[k]]$Censor == 0 & d[[k]]$Phase == 1 & d[[k]]$trun == F)], parm[1], log = T)) +
+        sum(dexp(d[[k]]$MBF[which(d[[k]]$Censor == 0 & d[[k]]$Phase == 2 & d[[k]]$trun == F)], parm[1]*parm[2], log = T)) +
+        sum(dexp(d[[k]]$MBF[which(d[[k]]$Censor == 0 & d[[k]]$Phase == 3 & d[[k]]$trun == F)], parm[1]*parm[2]*parm[3], log = T)) -
+        sum(parm[1]*(d[[k]]$MBF[which(d[[k]]$Censor == 1 & d[[k]]$Phase == 1 & d[[k]]$trun == F)])) -
+        sum(parm[1]*parm[4]*(d[[k]]$MBF[which(d[[k]]$Censor == 1 & d[[k]]$Phase == 2 & d[[k]]$trun == F)])) -
+        sum(parm[1]*parm[4]*parm[5]*(d[[k]]$MBF[which(d[[k]]$Censor == 1 & d[[k]]$Phase == 3 & d[[k]]$trun == F)])) +
+        sum(parm[1]*(d[[k]]$Lower[which(d[[k]]$Phase == 1 & d[[k]]$trun == T)]) - 
+              parm[1]*(d[[k]]$Upper[which(d[[k]]$Phase == 1 & d[[k]]$trun == T)])) +
+        sum(parm[1]*parm[2]*(d[[k]]$Lower[which(d[[k]]$Phase == 2 & d[[k]]$trun == T)]) - 
+              parm[1]*parm[2]*(d[[k]]$Upper[which(d[[k]]$Phase == 2 & d[[k]]$trun == T)])) + 
+        sum(parm[1]*parm[2]*parm[3]*(d[[k]]$Lower[which(d[[k]]$Phase == 3 & d[[k]]$trun == T)]) - 
+              parm[1]*parm[2]*parm[3]*(d[[k]]$Upper[which(d[[k]]$Phase == 3 & d[[k]]$trun == T)])) 
+    }
+    
+    lp <- lp + 
+      dgamma(parm[1], lamPriorA, lamPriorB, log=T) + 
+      dgamma(parm[2],hyperT1A, hyperT1B, log=T) +
+      dgamma(parm[3],hyperT2A, hyperT2B, log=T)
+    
+    return(lp)
+  }
+  
+  # phase obs counts 
+  phase2Count <- 0
+  phase3Count <- 0
+  for(i in 1:length(data)){
+    phase2Count <- phase2Count + length(which(data[[i]]$Phase == 2 & data[[i]]$Censor == 0))
+    phase3Count <- phase3Count + length(which(data[[i]]$Phase == 3 & data[[i]]$Censor == 0))
+  }
+  
+  noCenCount <- 0
+  for(i in 1:length(data)){
+    noCenCount <- noCenCount + sum(data[[i]]$Censor == 0)
+  }
+  
+  # MCMC draws
+  for (i in 2:samples) {
+    
+    datSum <- 0
+    for(j in 1:length(data)){
+      datSum <- datSum + sum(data[[j]]$MBF[which(data[[j]]$Phase == 1)],
+                             (theta1_draws[i-1] * data[[j]]$MBF[which(data[[j]]$Phase == 2)]),
+                             (theta1_draws[i-1] * theta2_draws[i-1] * data[[j]]$MBF[which(data[[j]]$Phase == 3)]))
+    }
+    
+    lam_draws[i] <- rgamma(1, noCenCount + lamPriorA, datSum + lamPriorB)
+
+    # Phase sums
+    phase2Sum <- 0
+    phase3Sum4 <- 0
+    phase3Sum5 <- 0
+    for(k in 1:length(data)){
+      phase2Sum <- phase2Sum + lam_draws[i] * sum(data[[k]]$MBF[which(data[[k]]$Phase == 2)])
+      
+      # for the first theta
+      phase3Sum4 <- phase3Sum4 + theta2_draws[i-1] * lam_draws[i] * sum(data[[k]]$MBF[which(data[[k]]$Phase == 3)])
+      
+      # for second theta
+      phase3Sum5 <- phase3Sum5 + theta1_draws[i-1] * lam_draws[i] * sum(data[[k]]$MBF[which(data[[k]]$Phase == 3)])
+    }
+    
+    theta1_draws[i] <- rgamma(1, phase2Count + phase3Count + theta1PriorA, phase2Sum + phase3Sum4 + theta1PriorB)
+    theta2_draws[i] <- rgamma(1, phase3Count + theta2PriorA, phase3Sum5 + theta2PriorB)
+  }
+  
+  lam_finalDraws <- lam_draws[seq(from = burnin + 1, to = samples, by = thin)]
+  theta1_finalDraws <- theta1_draws[seq(from = burnin + 1, to = samples, by = thin)]
+  theta2_finalDraws <- theta2_draws[seq(from = burnin + 1, to = samples, by = thin)]
+  
+  # DIC
+  d <- rep(0, length(lam_finalDraws))
+  for(i in 1:length(lam_finalDraws)){
+    for(j in 1:length(data)){
+      d[i] <- d[i] - 
+        2*(sum(dexp(data[[j]]$MBF[which(data[[j]]$Censor == 0 & data[[j]]$Phase == 1 & data[[j]]$trun == F)], lam_finalDraws[i], log = T)) +
+             sum(dexp(data[[j]]$MBF[which(data[[j]]$Censor == 0 & data[[j]]$Phase == 2 & data[[j]]$trun == F)], lam_finalDraws[i]*theta1_finalDraws[i], log = T)) +
+             sum(dexp(data[[j]]$MBF[which(data[[j]]$Censor == 0 & data[[j]]$Phase == 3 & data[[j]]$trun == F)], lam_finalDraws[i]*theta1_finalDraws[i]*theta2_finalDraws[i], log = T)) -
+             sum(lam_finalDraws[i]*(data[[j]]$MBF[which(data[[j]]$Censor == 1 & data[[j]]$Phase == 1 & data[[j]]$trun == F)])) -
+             sum(lam_finalDraws[i]*theta1_finalDraws[i]*(data[[j]]$MBF[which(data[[j]]$Censor == 1 & data[[j]]$Phase == 2 & data[[j]]$trun == F)])) -
+             sum(lam_finalDraws[i]*theta1_finalDraws[i]*theta2_finalDraws[i]*(data[[j]]$MBF[which(data[[j]]$Censor == 1 & data[[j]]$Phase == 3 & data[[j]]$trun == F)])) +
+             sum(lam_finalDraws[i]*(data[[j]]$Lower[which(data[[j]]$Phase == 1 & data[[j]]$trun == T)]) - 
+                   lam_finalDraws[i]*(data[[j]]$Upper[which(data[[j]]$Phase == 1 & data[[j]]$trun == T)])) +
+             sum(lam_finalDraws[i]*theta1_finalDraws[i]*(data[[j]]$Lower[which(data[[j]]$Phase == 2 & data[[j]]$trun == T)]) - 
+                   lam_finalDraws[i]*theta1_finalDraws[i]*(data[[j]]$Upper[which(data[[j]]$Phase == 2 & data[[j]]$trun == T)])) + 
+             sum(lam_finalDraws[i]*theta1_finalDraws[i]*theta2_finalDraws[i]*(data[[j]]$Lower[which(data[[j]]$Phase == 3 & data[[j]]$trun == T)]) - 
+                   lam_finalDraws[i]*theta1_finalDraws[i]*theta2_finalDraws[i]*(data[[j]]$Upper[which(data[[j]]$Phase == 3 & data[[j]]$trun == T)])))
+    }
+  }
+  
+  davg <- mean(d)
+  dthetahat <- 0
+  for(j in 1:length(data)){
+    dthetahat <- dthetahat - 2*(sum(dexp(data[[j]]$MBF[which(data[[j]]$Censor == 0 & data[[j]]$Phase == 1 & data[[j]]$trun == F)], mean(lam_finalDraws), log = T)) +
+                                  sum(dexp(data[[j]]$MBF[which(data[[j]]$Censor == 0 & data[[j]]$Phase == 2 & data[[j]]$trun == F)], mean(lam_finalDraws)*mean(theta1_finalDraws), log = T)) +
+                                  sum(dexp(data[[j]]$MBF[which(data[[j]]$Censor == 0 & data[[j]]$Phase == 3 & data[[j]]$trun == F)], mean(lam_finalDraws)*mean(theta1_finalDraws)*mean(theta2_finalDraws), log = T)) -
+                                  sum(mean(lam_finalDraws)*(data[[j]]$MBF[which(data[[j]]$Censor == 1 & data[[j]]$Phase == 1 & data[[j]]$trun == F)])) -
+                                  sum(mean(lam_finalDraws)*mean(theta1_finalDraws)*(data[[j]]$MBF[which(data[[j]]$Censor == 1 & data[[j]]$Phase == 2 & data[[j]]$trun == F)])) -
+                                  sum(mean(lam_finalDraws)*mean(theta1_finalDraws)*mean(theta2_finalDraws)*(data[[j]]$MBF[which(data[[j]]$Censor == 1 & data[[j]]$Phase == 3 & data[[j]]$trun == F)])) +
+                                  sum(mean(lam_finalDraws)*(data[[j]]$Lower[which(data[[j]]$Phase == 1 & data[[j]]$trun == T)]) - 
+                                        mean(lam_finalDraws)*(data[[j]]$Upper[which(data[[j]]$Phase == 1 & data[[j]]$trun == T)])) +
+                                  sum(mean(lam_finalDraws)*mean(theta1_finalDraws)*(data[[j]]$Lower[which(data[[j]]$Phase == 2 & data[[j]]$trun == T)]) - 
+                                        mean(lam_finalDraws)*mean(theta1_finalDraws)*(data[[j]]$Upper[which(data[[j]]$Phase == 2 & data[[j]]$trun == T)])) + 
+                                  sum(mean(lam_finalDraws)*mean(theta1_finalDraws)*mean(theta2_finalDraws)*(data[[j]]$Lower[which(data[[j]]$Phase == 3 & data[[j]]$trun == T)]) - 
+                                        mean(lam_finalDraws)*mean(theta1_finalDraws)*mean(theta2_finalDraws)*(data[[j]]$Upper[which(data[[j]]$Phase == 3 & data[[j]]$trun == T)])))
+  }
+  
+  pd <- davg - dthetahat
+  dic <- davg + pd
+  
+  return(list(lam_draws = lam_finalDraws,
+              theta1_draws = theta1_finalDraws,
+              theta2_draws = theta2_finalDraws,
+              DIC = dic,
+              PD = pd,
+              Deviance = d))
+}
---
+++ test2.R
@@ -0,0 +1,74 @@
+library(coda)
+
+source("simpleExpo.R")
+
+################################################################################
+# JLTV Data Good As New testing
+################################################################################
+JLTVData <- read.csv("JLTVData.csv")
+
+JLTVData[, 1] <- as.numeric(JLTVData[, 1])
+JLTVData$trun <- JLTVData$MBF == 0
+
+JLTVData$Lower <- 0
+JLTVData$Upper <- 0
+
+subs <- list()
+
+for(i in 1:26){
+  subs[[i]] <- JLTVData[which(JLTVData[,9] == i),]
+}
+
+subListGAN <- list()
+
+for(i in 1:26){
+  subListGAN[[i]] <- list()
+  for(j in 1:8){
+    subListGAN[[i]][[j]] <- subs[[i]][which(subs[[i]][,1]==j),c(6,8,3,10:12)]
+  }
+}
+
+for(i in 1:length(subListGAN)){
+  for(j in 1:length(subListGAN[[i]]))
+    if(all(subListGAN[[i]][[j]]$trun == F)){
+    } else {
+      for(k in 1:nrow(subListGAN[[i]][[j]]))
+        if(subListGAN[[i]][[j]][k,4] == T){
+          if(k == 1){
+          } else {
+            
+            if(subListGAN[[i]][[j]][k-1,2] == 1 & subListGAN[[i]][[j]][k-1,3] != 3){
+              subListGAN[[i]][[j]][k,3] <- subListGAN[[i]][[j]][k-1,3] + 1
+            }
+            
+            if(subListGAN[[i]][[j]][k+1,4] == F){
+              subListGAN[[i]][[j]][k,6] <- subListGAN[[i]][[j]][k+1,1]
+            } else if(subListGAN[[i]][[j]][k+2,4] == F){
+              subListGAN[[i]][[j]][k,6] <- subListGAN[[i]][[j]][k+2,1]
+            } else if(subListGAN[[i]][[j]][k+3,4] == F){
+              subListGAN[[i]][[j]][k,6] <- subListGAN[[i]][[j]][k+3,1]
+            } else if(subListGAN[[i]][[j]][k+4,4] == F){
+              subListGAN[[i]][[j]][k,6] <- subListGAN[[i]][[j]][k+4,1]
+            } else if(subListGAN[[i]][[j]][k+5,4] == F){
+              subListGAN[[i]][[j]][k,6] <- subListGAN[[i]][[j]][k+5,1]
+            } else{
+              subListGAN[[i]][[j]][k,6] <- subListGAN[[i]][[j]][k+6,1]
+            }
+          }
+        }
+    }
+}
+
+################################################################################
+# Expo test
+################################################################################
+
+expoResults <- simpExpoMCMC(subListGAN[[20]], 10000, burnin = 2000)
+
+expoResults$DIC
+expoResults$PD
+
+plot(as.mcmc(expoResults$lam_draws))
+plot(as.mcmc(expoResults$theta1_draws))
+plot(as.mcmc(expoResults$theta2_draws))
+
